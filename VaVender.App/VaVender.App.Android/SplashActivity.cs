643c11b22afdbfde565886926e00e0a99f244aa5
Jônatas Freitas <jonatasbmf@gmail.com>
2021-04-03 15:43:07 -0300

    Primeiro commit, iniciando projeto

---
+++ VaVender.App/VaVender.App.Android/SplashActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaVender.App.Droid
+{
+    [Activity(Label = "Va Vender!", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+            OverridePendingTransition(0, 0);
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await Task.Delay(1000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
